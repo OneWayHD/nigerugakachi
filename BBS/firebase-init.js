b7acd8f558860ce4a656b76316c9b5433b81a148
OneWayHD <oneway.holdingz@gmail.com>
2025-06-25 10:56:46 +0900

    Add files via upload

--- BBS/firebase-init.js
+++ BBS/firebase-init.js
@@ -3,12 +3,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-storage.js";
 
-// あなたのFirebase設定
+// Firebase構成情報（修正済み）
 const firebaseConfig = {
   apiKey: "AIzaSyC6yt0JKbXQhQICiU3skCQYV3yBe7TrLNc",
   authDomain: "nigerugakachi-d39ed.firebaseapp.com",
   projectId: "nigerugakachi-d39ed",
-  storageBucket: "nigerugakachi-d39ed.firebasestorage.app",
+  storageBucket: "nigerugakachi-d39ed.appspot.com", // ← ✅ 修正ポイント
   messagingSenderId: "61546555762",
   appId: "1:61546555762:web:e950e83c5608c0ef5455eb"
 };
